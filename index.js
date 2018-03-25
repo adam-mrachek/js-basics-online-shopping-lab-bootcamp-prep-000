a4ba27602005449500e4184cca685216ea355fae
Adam Mrachek <amrachek@outlook.com>
2018-03-25 19:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  var orderCost = 0;
+  var orderCost = total();
   if (cardNumber === undefined) {
     return "Sorry, we don\'t have a credit card on file for you.";
   }
