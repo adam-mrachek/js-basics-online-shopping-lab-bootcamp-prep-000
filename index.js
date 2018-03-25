312526b11a1e9ed8739ace0a3448ab57c1bd0151
Adam Mrachek <amrachek@outlook.com>
2018-03-25 18:47:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1);
+      return;
+    } else {
+      return "That item is not in your cart.";
     }
   }
 }
