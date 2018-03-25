fbda7410fc6a954bcaeb19552f1483ae87ed6b5d
Adam Mrachek <amrachek@outlook.com>
2018-03-25 18:16:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var cartContents = "In your cart, you have ";
+  for (var i = 0; i < cart.length; i++) {
+    cartContents += `${cart[i].itemName} and ${cart[i].itemPrice}, `
+  }
 }
 
 function total() {
